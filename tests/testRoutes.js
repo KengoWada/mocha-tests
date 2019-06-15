43b1de9da07d8839bd34abce7892756c2042cfb4
KengoWada <louiskengo3@gmail.com>
2019-06-15 16:41:59 +0300

    Updated repo dependencies

--- tests/testRoutes.js
+++ tests/testRoutes.js
@@ -1,28 +1,28 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
-const app = require("../app");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
 
 chai.use(chaiHttp);
 
-describe("Get response.", () => {
-	it("This one should fail.", () => {
+describe('Get response.', () => {
+	it('This one should fail.', () => {
 		return chai
 			.request(app)
-			.get("/")
+			.get('/')
 			.then(res => {
-				chai.expect(res.body.message).to.not.equal("me");
+				chai.expect(res.body.message).to.not.equal('me');
 			})
 			.catch(err => {
 				throw err;
 			});
 	});
 
-	it("This one should pass", () => {
+	it('This one should pass', () => {
 		return chai
 			.request(app)
-			.get("/")
+			.get('/')
 			.then(res => {
-				chai.expect(res.body.message).to.equal("We have launched");
+				chai.expect(res.body.message).to.equal('We have launched');
 			})
 			.catch(err => {
 				throw err;
